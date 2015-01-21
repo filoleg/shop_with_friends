21b33d99925e70eb694feb34eabcda790119b771
Oleg Filatov <ofilatov3@gatech.edu>
2015-01-21 13:45:43 -0500

    updated readme.p1.txt + put my name in AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -59,7 +59,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
 		  Person1 p1 = new Person1("P1");
-		  toPrint += p1.toString("gburdell1");
+		  toPrint += p1.toString("Oleg Filatov");
 		  
                   //Person2 replace P2 with your name
 		  //and gburdell with your gt id
