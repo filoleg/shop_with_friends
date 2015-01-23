25f7e126f1b5fae460265473dd6251b704e7379c
Christopher Tam <lightningstalon@gmail.com>
2015-01-23 00:27:05 -0500

    Completed Person5.java and Person6.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -31,7 +31,7 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+	  return input.substring(2, input.length() - 2) + input.substring(input.length() - 2, input.length());
 	}
 	
 	/**
